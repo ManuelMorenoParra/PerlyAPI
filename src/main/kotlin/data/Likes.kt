4f1919bfba04f292c473bc5bf22e6416f5802439
Manuel Moreno <manmorpar@alu.edu.gva.es>
2026-02-26 16:14:38 +0100

    id de likes

--- src/main/kotlin/data/Likes.kt
+++ src/main/kotlin/data/Likes.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.sql.javatime.datetime
 import java.time.LocalDateTime
 
 object Likes : Table("likes") {
-    val id = integer("id_like").autoIncrement()
+    val id = integer("id").autoIncrement()
     val idUsuario = integer("id_usuario")
     val idPublicacion = integer("id_publicacion")
     // Usamos LocalDateTime de Java para la compatibilidad con exposed-java-time

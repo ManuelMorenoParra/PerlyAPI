b5d285c40fb896e9148b0da9089f51a02dd903ab
Manuel Moreno <manuelmorenoparra4@gmail.com>
2026-02-28 17:28:41 +0100

    comentarios

--- src/main/kotlin/data/Publicaciones.kt
+++ src/main/kotlin/data/Publicaciones.kt
@@ -4,12 +4,12 @@ import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
 
 object Publicaciones : Table("publicaciones") {
-    val id = integer("id").autoIncrement() // Ajustado a "id" según el script SQL
+    val id = integer("id").autoIncrement()
     val idUsuario = integer("id_usuario")
     val texto = text("texto")
     val fecha = datetime("fecha")
-    val imagen = text("imagen").nullable() // Cambiado de blob a text para Base64
-    val idRetoVinculado = integer("id_reto_vinculado").nullable() // Nuevo atributo
+    val imagen = text("imagen").nullable()
+    val idRetoVinculado = integer("id_reto_vinculado").nullable()
 
     override val primaryKey = PrimaryKey(id)
 }
